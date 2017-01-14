e0a7b99cf0f48764c19c8ec617cf2ea1985e4a55
Josh Kuhar <jdkuhar@gmail.com>
2017-01-14 18:33:56 -0500

    added r endpoint

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(req, res) {
     res.send("Hello World");
 });
 
+app.get('/r', function(req, res) {
+    res.send("Hello World-r");
+});
+
 app.listen(process.env.PORT || 8080, process.env.IP);
